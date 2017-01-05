b39e3cc7a6023f82a4aedb43be75960f6ee40620
Dlee99 <Dereklee9ab@aol.com>
2017-01-04 20:15:39 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+
+class Customer {
+    Scanner sc = new Scanner(System.in);
+    private int SSN;
+    public int visits, weight;
+    public Customer(int ss){
+        SSN = ss;
+        Visit();
+    }
+    public void Visit(){
+        System.out.println("How much does the customer weigh?");
+        visits++;
+        weight = sc.nextInt();
+    }
+    public int getSSN(){
+        return SSN;
+    }
+}
+class Sale{
+
+}

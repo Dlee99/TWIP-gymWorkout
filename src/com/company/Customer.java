39fe76f611b64aded340dee32d303d12036674bf
Dlee99 <Dereklee9ab@aol.com>
2017-01-06 18:23:26 -0500

    lol did I do it right

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -6,7 +6,7 @@ public class Customer {
     Scanner sc = new Scanner(System.in);
     Scanner sc2 = new Scanner(System.in);
     private int SSN;
-    public double visits, weight, visitlng, hours, rate, buym, buycl, buyret, buyaq, buyp;
+    public double visits, weight, visitlng, hours, rate, buym, buycl, buyret, buyaq, buyp, cost, monthly;
     public boolean member = false;
     public Customer(int ss){
         SSN = ss;
@@ -14,6 +14,11 @@ public class Customer {
     }
     public Customer(){}
     public void Visit(){
+        buym = 0;
+        buycl = 0;
+        buyaq = 0;
+        buyp = 0;
+        monthly = 0;
         System.out.println("How much does the customer weigh?");
         visits++;
         weight = sc.nextDouble();
@@ -24,17 +29,6 @@ public class Customer {
                 break;
             }
             else if(y1.equals("N")){
-                String y2 = "";
-                while(true) {
-                    if (y2.equals("Y") && !member) {
-                        System.out.println("Membership purchased processed.");
-                        break;
-                    } else if(y2.equals("N")) {
-                        System.out.println("Membership not purchased.");
-                        break;
-                    }
-                    System.out.println("Would the customer like to purchase a membership? (Y) or (N)");
-                }
                 member = false;
                 break;
             }
@@ -43,7 +37,74 @@ public class Customer {
                 y1 = sc2.nextLine().toUpperCase();
             }
         }
-
+        String buy = "";
+        while(true){
+            if(buy.equals("membership")){
+                if(getMember()){
+                    System.out.println("Customer already has a membership.");
+                }
+                else{
+                    buym++;
+                }
+            }
+            else if(buy.equals("classes")){
+                buycl++;
+            }
+            else if(buy.equals("retail")){
+                System.out.println("Sorry, no retail is available for purchase right now.");
+                //sout("What items from retail?");
+                //while(true){
+                // if(buy2.equals("<item>"){
+                // int <item>++;
+                // }
+                // }
+                //double buyret = (<price> * <item>) + (<price2> * <item2>);
+            }
+            else if(buy.equals("aquatics")){
+                if(getMember()){
+                    buyaq++;
+                }
+                else{
+                    System.out.println("Non-members may not buy aquatics!");
+                }
+            }
+            else if(buy.equals("personal trainer")){
+                System.out.println("How many hours would you like to have a personal trainer for?");
+                double hr = sc.nextDouble();
+                buyp = buyp + hr;
+            }
+            else if(buy.equals("nothing")){
+                System.out.println("Thank you for shopping!");
+                break;
+            }
+            else{
+                System.out.println("What would you like to purchase?\n" +
+                        "Membership\n" +
+                        "Classes\n" +
+                        "Retail\n" +
+                        "Aquatics\n" +
+                        "Personal Trainer\n" +
+                        "Nothing");
+                buy = sc2.nextLine().toLowerCase();
+            }
+        }
+        System.out.println("That will cost: $" + getCost() + "\nAnd a monthly rate of: $" + getMonthly());
+        System.out.println("Customer");
+    }
+    public double getMonthly(){
+        if(getMember()){
+            monthly = 65;
+        }
+        return monthly;
+    }
+    public double getCost(){
+        if(getMember()){
+            cost = (8 * buycl) + (10 * buyaq) + (20 * buyp);
+        }
+        else{
+            cost = (10 * buycl) + (25 * buyp);
+        }
+        return cost;
     }
     public int getSSN(){
         return SSN;
--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -6,4 +6,19 @@ public class Employee extends Customer {
         SSN = ss;
         Visit();
     }
+    public double getCost(){
+        if(getMember()){
+            cost = (3 * buycl) + (7 * buyaq) + (15 * buyp) + (.8 * buyret);
+        }
+        else{
+            cost = (3 * buycl) + (7 * buyaq) + (15 * buyp) + (.9 * buyret);
+        }
+        return cost;
+    }
+    public double getMonthly(){
+        if(getMember()){
+            monthly = 1;
+        }
+        return monthly;
+    }
 }
